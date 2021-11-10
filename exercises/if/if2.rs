9fb6f32d0711e0a6e08210c70023b3712ff3b877
totaland <james.nguyen868@gmail.com>
2021-11-10 22:57:31 +1000

    WIP

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -4,13 +4,12 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute the command `rustlings hint if2` if you want a hint :)
 
-// I AM NOT DONE
 
 pub fn fizz_if_foo(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
     } else {
-        1
+        "baz"
     }
 }
 
@@ -26,7 +25,7 @@ mod tests {
 
     #[test]
     fn bar_for_fuzz() {
-        assert_eq!(fizz_if_foo("fuzz"), "bar")
+        assert_eq!(fizz_if_foo("fuzz"), "baz")
     }
 
     #[test]
--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,19 +2,18 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let vec1 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec0 = fill_vec(vec1);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
+    vec0.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
